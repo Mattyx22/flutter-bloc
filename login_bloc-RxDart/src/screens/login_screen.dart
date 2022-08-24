098e7468a96ca2f8224be203017cc800c1228a8d
Mattyx <mattyx22@gmail.com>
2022-08-24 16:58:20 +0200

    Add files via upload

---
+++ login_bloc-RxDart/src/screens/login_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    final bloc = Provider.of(context);
+
+    return Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            emailField(bloc),
+            passwordField(bloc),
+            Container(margin: const EdgeInsets.only(top: 25)),
+            submitButton(bloc),
+          ],
+        ));
+  }
+
+  Widget emailField(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.email,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changeEmail,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'you@example.com',
+              labelText: "Email Address",
+              errorText: snapshot.error?.toString(),
+            ),
+          );
+        });
+  }
+
+  Widget passwordField(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.password,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: bloc.changePassword,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              labelText: 'Password',
+              errorText: snapshot.error?.toString(),
+            ),
+            obscureText: true,
+          );
+        });
+  }
+
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+        stream: bloc.submitValid,
+        builder: (context, snapshot) {
+          return ElevatedButton(
+            onPressed: snapshot.hasData ? bloc.submit : null,
+            child: const Text('Login'),
+          );
+        });
+  }
+}
